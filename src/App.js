695d6fc0c80531f2afe2fa2a46315a4d99c60e11
felipetfernandes <felipe.tavares.fernandes@gmail.com>
2023-01-02 15:38:32 -0300

    01 and 02 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Album from './services/pages/Album';
+import Favorites from './services/pages/Favorites';
+import Login from './services/pages/Login';
+import Profile from './services/pages/Profile';
+import ProfileEdit from './services/pages/ProfileEdit';
+import Search from './services/pages/Search';
+import NotFound from './services/pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/services/pages/Login.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../userAPI';
+import Load from '../../components/Load';
+
+export default class Login extends Component {
+  state = {
+    userName: '',
+    buttonDisable: true,
+    load: false,
+  };
+
+  onInputChange = ({ target: { value } }) => {
+    this.setState({ userName: value }, () => {
+      this.buttonChange();
+    });
+  };
+
+  buttonChange = () => {
+    const { userName } = this.state;
+    const minLength = 3;
+    if (userName.length >= minLength) {
+      this.setState({ buttonDisable: false });
+    } else {
+      this.setState({ buttonDisable: true });
+    }
+  };
+
+  buttonSubmit = () => {
+    const { userName } = this.state;
+    const { history } = this.props;
+    this.setState({ load: true }, async () => {
+      await createUser({ name: userName });
+      return history.push('/search');
+    });
+  };
+
+  render() {
+    const { userName, buttonDisable, load } = this.state;
+    return (
+      (load) ? <Load />
+        : (
+          <form data-testid="page-login">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              placeholder="User Name"
+              value={ userName }
+              onChange={ this.onInputChange }
+            />
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ buttonDisable }
+              onClick={ this.buttonSubmit }
+            >
+              Entrar
+            </button>
+          </form>
+        )
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
